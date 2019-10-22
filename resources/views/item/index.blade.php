b7ed41e495f54249b0af0abff1aa114f7b2df87d
Giovanni Buffa <tech@giovannibuffa.it>
2019-10-22 16:12:32 +0200

    Refactor index view for menu tree

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -1,11 +1,9 @@
 @extends('layout')
 
 @section('content')
-  <div class="flex items-center mb-3">
-    <h1 class="w-full text-center mb-2 md:mb-0 md:text-left md:w-auto md:flex-1">Menu items <small class="text-muted">({{ $menu['name'] }} - {{ $menu['locale'] }})</small></h1>
-  </div>
-
-  <menu-item-index :items="{{ json_encode($menu['items']) }}" menu="{{ $menu['slug'] }}"></menu-item-index>
-
-  <menu-item-create locale="{{ $menu['locale'] }}" menu="{{ $menu['slug'] }}"></menu-item-create>
+  <menu-item-index
+    :items="{{ json_encode($menu['items']) }}"
+    menu="{{ $menu['slug'] }}"
+    title="{{ $menu['name'] }}"
+    locale="{{ $menu['locale'] }}"></menu-item-index>
 @endsection
